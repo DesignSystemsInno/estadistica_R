1e7008a85e33d5ba6724fae9bdb69a4a69ffd89d
Jairo Carvajal <desingsystem2020@gmail.com>
2021-01-28 17:59:00 -0500

    Taller 1

---
+++ taller.r
@@ -0,0 +1,16 @@
+# Problema 1 vectores
+(h = c(1,2,3,4))
+(y = seq(1,10)) #Secuencia del 1 al 10"
+(z = seq(1,10,2)) #Secuencia del 1 al 10 con incremenento par
+(w = rep(0,10)) #Vector de tamaño 10 con valores 0
+
+y[3] = 2
+
+
+(h = matrix(seq(1,8),4,byrow=TRUE))
+
+(x=seq(0,10,by=0.01))
+
+(plot(x,log(x)))
+
+
